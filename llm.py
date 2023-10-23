f9c339a848a4159c795a911ad2556b6d1a0fe810
Afif Malghani <afifulhasnain@gmail.com>
2023-10-23 09:24:56 +0500

    changes required for dockerization

--- llm.py
+++ llm.py
@@ -1,6 +1,6 @@
 # import
 from langchain.llms import HuggingFacePipeline
-
+from huggingface_hub import hf_hub_download
 
 # Local CTransformers wrapper for Llama-2-7B-Chat
 from langchain.llms import CTransformers
@@ -17,5 +17,10 @@ except Exception as e:
     print(
         "\nFailed to load model: \n",
         e,
-        "\nMake sure that a model.bin file is peresent in the models directory.\n",
+        "\nDownlaoding llama-2-7b-chat.ggmlv3.q8_0.bin \n",
+    )
+    hf_hub_download(
+        repo_id="TheBloke/Llama-2-7B-Chat-GGML"
+        filename="llama-2-7b-chat.ggmlv3.q8_0.bin",
+        local_dir="models",
     )
